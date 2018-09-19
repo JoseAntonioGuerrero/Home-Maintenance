6e822966bd3857100f769285d015d8eed7914e02
Jose Guerrero <joseaguerrero6@gmail.com>
2018-09-19 15:07:33 -0400

    Creating search component and setting up router module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BuscarComponent } from './buscar/buscar.component';
+
+const routes: Routes = [
+  { path: 'buscar', component: BuscarComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+
+})
+
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
